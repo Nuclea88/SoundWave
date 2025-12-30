1b28c6b57f20495ce9e19aea087c1f02d304c114
Nuclea88 <ingrid.lopov@gmail.com>
2025-12-30 21:34:24 +0100

    feat: create routes with layout to home, discover and login

---
+++ src/router/index.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router";
+import Home from "../pages/Home";
+import Discover from "../pages/Discover";
+import Login from "../pages/Login";
+import Layout from "../pages/Layout";
+
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            { index: true, element: <Home /> },
+            { path: "Discover" , element: <Discover /> },
+            { path: "Login", element: <Login /> }    
+]
+    }
+]);
