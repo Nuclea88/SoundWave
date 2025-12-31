ddc577ead464b716cc18f90e8b64acd89584d197
Nuclea88 <ingrid.lopov@gmail.com>
2025-12-31 14:18:43 +0100

    feat: create footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return(
+        <footer>
+            <nav>
+                <p>About Us</p>
+                <p>Contact</p>
+                <img src="/images/twitter.svg" alt = "twitter logo" />
+                <p>Twitter</p>
+                <img src="/images/facebook.svg" alt = "facebook logo" />
+                <p>Facebook</p>
+            </nav>
+        </footer>
+    );
+};
+export default Footer;
